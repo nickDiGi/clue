3d0e2e57e64526d882e31eec4bf0754b8e05b34e
digiovann9 <48811597+nickDiGi@users.noreply.github.com>
2024-03-15 18:03:26 -0400

    Updates to add initial text based lobby system

--- clue-client.py
+++ clue-client.py
@@ -1,20 +1,58 @@
+import socket
+import pickle
+import threading
+from enum import Enum
+
+'''
+Classes used to format messages
+'''
+class Message:
+    def __init__(self, message_type, sender_id, data):
+        self.message_type = message_type
+        self.sender_id = sender_id
+        self.data = data
+
+class Message_Types(Enum):
+    CREATE_GAME = 1
+    JOIN_GAME = 2
+    LOBBY_ROSTER_UPDATE = 3
+
 '''
 Functions for displaying GUI elements
 '''
 def show_main_menu():
     # Create and display GUI elements for the main menu
     # Should include a button for "Create New Game" and a button for "Join Game"
+
+    # Text based version
+    print("Welcome to Clue!")
+    print("Do you want to create a new game or join an existing one?")
+    print("1. Create a new game")
+    print("2. Join an existing game")
+
+    while True:
+        option = input("Enter your choice (1 or 2): ")
+        if option in ('1', '2'):
+            return option
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+def show_create_game_popup():
+    # Create and display GUI elements for the pop-up that appears after you select "Join Game"
+    # Should include a space to ender in the game ID of the game you want to join
+
+    # Text based version
+    return input("Enter your name: ")
+
+def show_join_game_popup():
+    # Create and display GUI elements for the pop-up that appears after you select "Join Game"
+    # Should include a space to ender in the game ID of the game you want to join
     pass
 
 def show_lobby_menu():
     # Create and display GUI elements for the lobby menu
     # Should include a list of players in the game and some sort of "Start" or "Ready Up" button
     # Should display game ID
-    pass
-
-def show_join_game_popup():
-    # Create and display GUI elements for the pop-up that appears after you select "Join Game"
-    # Should include a space to ender in the game ID of the game you want to join
     pass
 
 def show_loading_screen(message):
@@ -70,19 +108,65 @@ def disable_controls():
 '''
 Functions for messaging
 '''
-def send_message(message):
+def send_message(message, host='localhost', port=12345):
     # Apply any additional formatting and send message to server
-    pass
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
 
-def process_message():
+            # Serialize the message object using pickle
+            serialized_message = pickle.dumps(message)
+            # Prefix the serialized message with its length
+            message_length = len(serialized_message)
+            s.sendall(message_length.to_bytes(4, byteorder='big'))
+            s.sendall(serialized_message)
+        print("Sent")
+    except Exception as e:
+        print("Error occurred while sending message:", e)
+
+def process_message(host='localhost', port=12346):
     # Process message received from server and call functions to update the GUI accordingly
-    pass
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((host, port))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                # Receive the message length
+                message_length_bytes = conn.recv(4)
+                message_length = int.from_bytes(message_length_bytes, byteorder='big')
+                # Receive the serialized message
+                serialized_message = b''
+                while len(serialized_message) < message_length:
+                    serialized_message += conn.recv(message_length - len(serialized_message))
+                # Deserialize the message object using pickle
+                message = pickle.loads(serialized_message)
+                print('Received message:')
+                print('Type:', message.message_type)
+                print('Sender ID:', message.sender_id)
+                print('Data:', message.data)
 
 '''
 Main
 '''
 def main():
-    pass
+
+    # Start receiving messages in a separate thread
+    receive_message_thread = threading.Thread(target=process_message)
+    receive_message_thread.daemon = True
+    receive_message_thread.start()
+
+    while True: # TODO: This loop is for testing and demonstration
+        choice = show_main_menu()
+        if choice == '1':
+            player_name = show_create_game_popup()
+            create_game_message =  Message(Message_Types.CREATE_GAME, player_name, None)
+            send_message(create_game_message)
+            # Call function to create a new game
+        else:
+            create_game_message =  Message(Message_Types.JOIN_GAME, "Temp-Name", 95417)
+            send_message(create_game_message)
 
 if __name__ == "__main__":
     main()
--- clue-server.py
+++ clue-server.py
@@ -1,5 +1,22 @@
 import random
+import socket
 from enum import Enum
+
+import pickle
+
+'''
+Classes used to format messages
+'''
+class Message:
+    def __init__(self, message_type, sender_id, data):
+        self.message_type = message_type
+        self.sender_id = sender_id
+        self.data = data
+
+class Message_Types(Enum):
+    CREATE_GAME = 1
+    JOIN_GAME = 2
+    LOBBY_ROSTER_UPDATE = 3
 
 '''
 Cards
@@ -36,13 +53,20 @@ class Room(Enum):
 Player
 '''
 class Player:
-    def __init__(self, name, cards):
+    def __init__(self, name, address, cards):
         self.name = name
+        self.address = address
         self.position = None
         self.cards = cards
 
+    def get_name(self):
+        return self.name
+
     def give_card(self, card):
         self.cards.append(card)
+
+    def get_address(self):
+        return self.address
 
     def set_position(self, position):
         self.position = position
@@ -54,15 +78,18 @@ class Player:
 Game-Logic Handler
 '''
 class Game_Logic:
-    def __init__(self, name):
-        players = [
-            Player(name, []),
+    def __init__(self, name, address):
+        self.players = [
+            Player(name, address, []),
         ]
-        id = random.randint(10000, 99999)
-        turn_number = 0
+        self.id = random.randint(10000, 99999)
+        self.turn_number = 0
 
-    def add_player(self, name):
-        self.players.append(Player(name, []))
+    def add_player(self, name, address):
+        self.players.append(Player(name, address, []))
+
+    def get_players(self):
+        return self.players
 
     def get_id(self):
         return self.id
@@ -79,6 +106,9 @@ class Game_Logic:
         # Assign each player a random position
         # Can use Player.set_position function
         pass
+
+# Global dictionary to store ongoing games
+ongoing_games = {}
 
 '''
 Functions for handling game-state changes
@@ -104,18 +134,73 @@ def handle_player_action():
 '''
 Functions for messaging
 '''
-def send_message(message):
+def send_message(message, host, port=12346):
     # Apply any additional formatting and send message to client
-    pass
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
 
-def process_message():
+            # Serialize the message object using pickle
+            serialized_message = pickle.dumps(message)
+            # Prefix the serialized message with its length
+            message_length = len(serialized_message)
+            s.sendall(message_length.to_bytes(4, byteorder='big'))
+            s.sendall(serialized_message)
+        print("Sent")
+    except Exception as e:
+        print("Error occurred while sending message:", e)
+
+def process_message(host='localhost', port=12345):
     # Process message received from client and call functions to handle accordingly
-    pass
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((host, port))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+
+                # Receive the message length
+                message_length_bytes = conn.recv(4)
+                message_length = int.from_bytes(message_length_bytes, byteorder='big')
+                # Receive the serialized message
+                serialized_message = b''
+                while len(serialized_message) < message_length:
+                    serialized_message += conn.recv(message_length - len(serialized_message))
+                # Deserialize the message object using pickle
+                message = pickle.loads(serialized_message)
+                if (message.message_type == Message_Types.CREATE_GAME):
+                    # Create game
+                    game = Game_Logic(message.sender_id, addr)
+                    # Add game to the collection of games, with it's ID as it's key
+                    ongoing_games[game.get_id()] = game
+                    print("Added player " + message.sender_id + " to game with lobby ID " + str(game.get_id()))
+                    player_names = []
+                    for player in game.get_players():
+                            player_names.append(player.get_name())
+                    create_game_response =  Message(Message_Types.LOBBY_ROSTER_UPDATE, None, player_names)
+                    send_message(create_game_response, 'localhost')
+                    print("Sent create game response")
+
+                elif (message.message_type == Message_Types.JOIN_GAME):
+                    try:
+                        game = ongoing_games[message.data]
+                        print("That game exists!")
+                        game.add_player(addr, message.sender_id)
+                        for player in game.get_players():
+                            print(player.get_address())
+                    except:
+                        print("Sorry, we couldn't find that game")
+                else:
+                    print("Error, unknown message type received")
 
 '''
 Main
 '''
 def main():
+    # Start the server to receive the message
+    print("Server Started")
+    process_message()
     pass
 
 if __name__ == "__main__":

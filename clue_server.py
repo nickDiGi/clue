94164b21bcd7c4cd52adc61a3bb4d5ab3d0f2dea
digiovann9 <48811597+nickDiGi@users.noreply.github.com>
2024-04-12 13:20:39 -0400

    Cleaned up functions and fixed a bug.
    Fixed a bug where players would get messages in the order they started the application, not the order they joined the game.

--- clue_server.py
+++ clue_server.py
@@ -4,26 +4,167 @@ import pickle
 import socket
 import time # TODO Remove this
 
+# Global dictionary to store ongoing games
+ongoing_games = {}
+
 '''
 Functions for handling game-state changes
 '''
-def create_new_game():
-    # Generate a new game logic handler and call its init
-    # Add the game logic hander to the list of current game logic handlers
-    # Generate message with Game_Session game_id to send to client
-    pass
+# Creates a new game_logic object to hold game state info.
+# Then adds the creating player to the game they created.
+def create_new_game(message, addr):
+    global ongoing_games
 
-def join_game(game_id, player_id):
-    # Using the given game_id, find the game in the list of current games, and add the player to the roster
-    # Create a message notifying players in that game that someone has joined, and call send_message
-    # Call Game_Session functions to add_player(s), and once the lobby is full,
-    #   call functions to set_player_positions and deal_cards
-    # Generate a message each time a player joins to notify the other players
-    pass
+    print('\nCREATE GAME MESSAGE RECEIVED:')
+    print('Creating Player:', message.sender_id)
 
-def handle_player_action():
-    # This may need to get broken out into several functions
-    pass
+    game = clue_game_logic.Game_Session(message.sender_id, addr)
+    # Add game to the collection of games, with it's ID as it's key
+    ongoing_games[game.get_id()] = game
+    print("Added player " + message.sender_id + " to game with lobby ID " + str(game.get_id()))
+    player_names = []
+    for player in game.get_players():
+            player_names.append(player.get_name())
+    print("Players now the lobby: " + str(player_names))
+    create_game_response =  clue_messaging.Message(clue_messaging.Message_Types.LOBBY_ROSTER_UPDATE, game.get_id(), player_names, None)
+    # TODO: Replace port stuff with with a permanent solution
+    index = 0
+    for player in game.get_players():
+        send_message(create_game_response, player.get_address()[0], (12346+index))
+        print("Sent lobby update to " + player.get_name())
+        index = index + 1
+
+
+# Adds the player in the message to the game with the ID given in the message.
+def join_game(message, addr):
+    global ongoing_games
+
+    print('\nJOIN GAME MESSAGE RECEIVED:')
+    print('Joining Player:', message.sender_id)
+    print('Game ID to Join:', message.game_state_data)
+
+    try:
+        game = ongoing_games[message.game_state_data]
+        print("Game Found Matching Given ID")
+        game.add_player(message.sender_id, addr)
+        print("Added player " + message.sender_id + " to game with lobby ID " + str(game.get_id()))
+        player_names = []
+        for player in game.get_players():
+                player_names.append(player.get_name())
+        print("Players now the lobby: " + str(player_names))
+        create_game_response =  clue_messaging.Message(clue_messaging.Message_Types.LOBBY_ROSTER_UPDATE, game.get_id(), player_names, None)
+        # TODO: Replace port stuff with with a permanent solution
+        index = 0
+        for player in game.get_players():
+            send_message(create_game_response, player.get_address()[0], (12346+index))
+            print("Sent lobby update to " + player.get_name())
+            index = index + 1
+
+        # TODO: Make this happen after a start game message is received
+        if len(player_names) == 3:
+            # Deal cards and set starting positions
+            game.deal_cards()
+            game.set_player_positions()
+
+            # Parse list of players to grab info about their character and where they are on the board
+            players = game.get_players()
+            #board_info = ''
+            board_info = []
+            for player in players:
+                #board_info = board_info + ((player.get_name() + "(" + str(player.get_character()) + ") is currently in the " + str(player.get_position()) + "\n"))
+                stripped_player = clue_game_logic.Player(player.get_name(), None, [])
+                stripped_player.set_character(player.get_character())
+                stripped_player.set_position(player.get_position())
+                board_info.append(stripped_player)
+
+            # Send message to each player with the positions of other players and their own player info
+            index = 0
+            for player in players:
+                print("\n")
+                print(player)
+                game_state_update =  clue_messaging.Message(clue_messaging.Message_Types.GAME_STATE_UPDATE, game.get_id(), board_info, player)
+                send_message(game_state_update, player.get_address()[0], (12346+index)) # TODO: Fix port weirdness
+                print("Sent update to " + player.get_name())
+                # Notify the player who's turn it is that it is their turn
+                if player.turn_order_number == 1:
+                    time.sleep(.5) # TODO : Add threading and remove this
+                    turn_notification =  clue_messaging.Message(clue_messaging.Message_Types.YOUR_TURN_NOTIFICATION, game.get_id(), board_info, player)
+                    send_message(turn_notification, player.get_address()[0], (12346+index)) # TODO: Fix port weirdness
+                    print("Sent turn notification to " + player.get_name())
+                index = index + 1
+                time.sleep(.5) # TODO : Add threading and remove this
+                
+    except Exception as e:
+        print("Sorry, we couldn't find that game: ", e)
+
+# Updates an existing game_logic object (ongoing game) with new player info from the message.
+def update_game_state(message):
+    global ongoing_games
+
+    print('\nGAME STATE UPDATE RECEIVED:')
+    print('Game Session:', message.sender_id)
+    print('Player Data:', message.player_state_data)
+    game = ongoing_games[message.sender_id]
+    game_turn_number = game.get_turn_number() + 1
+    if game_turn_number > 3: # TODO: Make this 3 a 6
+        game_turn_number = 1
+    game.set_turn_number(game_turn_number)
+    players = game.get_players()
+
+    # Update the data for the player who sent the update
+    index = 0
+    while index < len(players):
+        player = players[index]
+        print("Player named " + player.get_name() + " is not player " + message.player_state_data.get_name())
+        if player.get_name() == message.player_state_data.get_name():
+            print("Just kidding! Updating the player data!")
+            players[index] = message.player_state_data
+            break # TODO Maybe get rid of this break
+        index = index + 1
+
+    board_info = []
+    for player in players:
+        #board_info = board_info + ((player.get_name() + "(" + str(player.get_character()) + ") is currently in the " + str(player.get_position()) + "\n"))
+        stripped_player = clue_game_logic.Player(player.get_name(), None, [])
+        stripped_player.set_character(player.get_character())
+        stripped_player.set_position(player.get_position())
+        board_info.append(stripped_player)
+
+    for player in players:
+        print("Player " + str(player.get_name()) + " is now in " + str(player.get_position()))
+
+    index = 0
+    for player in players:
+        game_state_update =  clue_messaging.Message(clue_messaging.Message_Types.GAME_STATE_UPDATE, game.get_id(), board_info, player)
+        send_message(game_state_update, player.get_address()[0], (12346+index)) # TODO: Fix port weirdness
+        print("Sent update to " + player.get_name())
+        index = index + 1
+        time.sleep(.5)
+
+    index = 0
+    for player in players:
+        if player.turn_order_number == game_turn_number:
+                    turn_notification =  clue_messaging.Message(clue_messaging.Message_Types.YOUR_TURN_NOTIFICATION, game.get_id(), board_info, player)
+                    send_message(turn_notification, player.get_address()[0], (12346+index)) # TODO: Fix port weirdness
+                    print("Sent turn notification to " + player.get_name())
+        index = index + 1
+
+# Handles server-side logic for when a player makes an accusation.
+def handle_accusation_action(message):
+    global ongoing_games
+
+    print('\nACCUSATION ACTION MESSAGE RECEIVED:')
+    print('Game Session:', message.sender_id)
+    print('Player Data:', message.player_state_data)
+    print('Their Guess: ' + str(message.game_state_data))
+
+    game = ongoing_games[message.sender_id]
+    winning_cards = game.get_winning_cards()
+    if message.game_state_data == winning_cards:
+        print("They won!")
+    else:
+        print("They lost!")
+
 
 '''
 Functions for messaging
@@ -44,8 +185,9 @@ def send_message(message, host, port):
     except Exception as e:
         print("Error occurred while sending message:", e)
 
+
+# Listen for and receive messages from clients and call function to process them
 def receive_message(host='localhost', port=12345):
-    # Process message received from client and call functions to handle accordingly
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((host, port))
@@ -95,151 +237,23 @@ def receive_message(host='localhost', port=12345):
     server.serve_forever()
                 
 
+# Validate message type, and pass to correct handling functions
 def process_message(message, addr):
     if (message.message_type == clue_messaging.Message_Types.CREATE_GAME):
-        # Create game
-        game = clue_game_logic.Game_Session(message.sender_id, addr)
-        # Add game to the collection of games, with it's ID as it's key
-        ongoing_games[game.get_id()] = game
-        print("Added player " + message.sender_id + " to game with lobby ID " + str(game.get_id()))
-        player_names = []
-        for player in game.get_players():
-                player_names.append(player.get_name())
-        print("Players now the lobby: " + str(player_names))
-        create_game_response =  clue_messaging.Message(clue_messaging.Message_Types.LOBBY_ROSTER_UPDATE, game.get_id(), player_names, None)
-        # TODO: Replace port stuff with with a permanent solution
-        index = 0
-        for player in game.get_players():
-            send_message(create_game_response, player.get_address()[0], (12346+index))
-            print("Sent lobby update to " + player.get_name())
-            index = index + 1
+        create_new_game(message, addr)
 
     elif (message.message_type == clue_messaging.Message_Types.JOIN_GAME):
-        try:
-            game = ongoing_games[message.game_state_data]
-            print("Game Found Matching Given ID")
-            game.add_player(message.sender_id, addr)
-            print("Added player " + message.sender_id + " to game with lobby ID " + str(game.get_id()))
-            player_names = []
-            for player in game.get_players():
-                    player_names.append(player.get_name())
-            print("Players now the lobby: " + str(player_names))
-            create_game_response =  clue_messaging.Message(clue_messaging.Message_Types.LOBBY_ROSTER_UPDATE, game.get_id(), player_names, None)
-            # TODO: Replace port stuff with with a permanent solution
-            index = 0
-            for player in game.get_players():
-                send_message(create_game_response, player.get_address()[0], (12346+index))
-                print("Sent lobby update to " + player.get_name())
-                index = index + 1
-
-            # TODO: Make this happen after a start game message is received
-            if len(player_names) == 3:
-                # Deal cards and set starting positions
-                game.deal_cards()
-                game.set_player_positions()
-
-                # Parse list of players to grab info about their character and where they are on the board
-                players = game.get_players()
-                #board_info = ''
-                board_info = []
-                for player in players:
-                    #board_info = board_info + ((player.get_name() + "(" + str(player.get_character()) + ") is currently in the " + str(player.get_position()) + "\n"))
-                    stripped_player = clue_game_logic.Player(player.get_name(), None, [])
-                    stripped_player.set_character(player.get_character())
-                    stripped_player.set_position(player.get_position())
-                    board_info.append(stripped_player)
-
-                # Send message to each player with the positions of other players and their own player info
-                index = 0
-                for player in players:
-                    print("\n")
-                    print(player)
-                    game_state_update =  clue_messaging.Message(clue_messaging.Message_Types.GAME_STATE_UPDATE, game.get_id(), board_info, player)
-                    send_message(game_state_update, player.get_address()[0], (12346+index)) # TODO: Fix port weirdness
-                    print("Sent update to " + player.get_name())
-                    # Notify the player who's turn it is that it is their turn
-                    if player.turn_order_number == 1:
-                        time.sleep(.5) # TODO : Add threading and remove this
-                        turn_notification =  clue_messaging.Message(clue_messaging.Message_Types.YOUR_TURN_NOTIFICATION, game.get_id(), board_info, player)
-                        send_message(turn_notification, player.get_address()[0], (12346+index)) # TODO: Fix port weirdness
-                        print("Sent turn notification to " + player.get_name())
-                    index = index + 1
-                    time.sleep(.5) # TODO : Add threading and remove this
-                    
-        except Exception as e:
-            print("Sorry, we couldn't find that game: ", e)
+        join_game(message, addr)
 
     elif (message.message_type == clue_messaging.Message_Types.GAME_STATE_UPDATE):
-        print('\nReceived update message from a client:')
-        print('Type:', message.message_type)
-        print('Game Session:', message.sender_id)
-        print('Player Data:', message.player_state_data)
-        game = ongoing_games[message.sender_id]
-        game_turn_number = game.get_turn_number() + 1
-        if game_turn_number > 3: # TODO: Make this 3 a 6
-            game_turn_number = 1
-        game.set_turn_number(game_turn_number)
-
-        players = game.get_players()
-
-        # Update the data for the player who sent the update
-        index = 0
-        while index < len(players):
-            player = players[index]
-            print("Player named " + player.get_name() + " is not player " + message.player_state_data.get_name())
-            if player.get_name() == message.player_state_data.get_name():
-                print("Just kidding! Updating the player data!")
-                players[index] = message.player_state_data
-                break # TODO Maybe get rid of this break
-            index = index + 1
-
-        board_info = []
-        for player in players:
-            #board_info = board_info + ((player.get_name() + "(" + str(player.get_character()) + ") is currently in the " + str(player.get_position()) + "\n"))
-            stripped_player = clue_game_logic.Player(player.get_name(), None, [])
-            stripped_player.set_character(player.get_character())
-            stripped_player.set_position(player.get_position())
-            board_info.append(stripped_player)
-
-        for player in players:
-            print("Player " + str(player.get_name()) + " is now in " + str(player.get_position()))
-
-        index = 0
-        for player in players:
-            game_state_update =  clue_messaging.Message(clue_messaging.Message_Types.GAME_STATE_UPDATE, game.get_id(), board_info, player)
-            send_message(game_state_update, player.get_address()[0], (12346+index)) # TODO: Fix port weirdness
-            print("Sent update to " + player.get_name())
-            index = index + 1
-            time.sleep(.5)
-
-        index = 0
-        for player in players:
-            if player.turn_order_number == game_turn_number:
-                        turn_notification =  clue_messaging.Message(clue_messaging.Message_Types.YOUR_TURN_NOTIFICATION, game.get_id(), board_info, player)
-                        send_message(turn_notification, player.get_address()[0], (12346+index)) # TODO: Fix port weirdness
-                        print("Sent turn notification to " + player.get_name())
-            index = index + 1
+        update_game_state(message)
 
     elif (message.message_type == clue_messaging.Message_Types.ACCUSATION_ACTION):
-        print('\nReceived update message from a client:')
-        print('Type:', message.message_type)
-        print('Game Session:', message.sender_id)
-        print('Player Data:', message.player_state_data)
-        print('Their guess: ' + str(message.game_state_data))
-
-        game = ongoing_games[message.sender_id]
-        winning_cards = game.get_winning_cards()
-
-        if message.game_state_data == winning_cards:
-            print("They won!")
-        else:
-            print("They lost!")
+        handle_accusation_action(message)
 
     else:
         print("Error, unknown message type received")
 
-# Global dictionary to store ongoing games
-ongoing_games = {}
 
 '''
 Main
